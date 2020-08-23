fe34e819dad0e1f3bdd176f56ca22989573bdb4b
maddiemoffat <maddiemoffat@hotmail.com>
2020-08-22 23:03:49 -0600

    adding array for enemy names. loop for fight function

--- assets/js/robot-gladiators.js
+++ assets/js/robot-gladiators.js
@@ -3,20 +3,25 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-//you can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+console.log(enemyNames.length);
+
+for(var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+  }
+
+  var fight = function(enemyName) {
     // Alert users that they are starting the round
         window.alert("Welcome to Robot Gladiators!");
         var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
     // if player choses to fight, then fight
-    if (promptFight === "fight" || promptFight === "FIGHT") 
+    if (promptFight === "fight" || promptFight === "FIGHT" || promptFight === "Fight") 
     {
         // remove enemy's health by subtracting the amount set in the playerAttack variable
         enemyHealth = enemyHealth - playerAttack;
@@ -66,4 +71,6 @@ var fight = function() {
     fight();
 };
 
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+  }

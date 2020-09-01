8c8daf1b766d55dbbfcc7e4426ee74420d0f51fc
maddiemoffat <maddiemoffat@hotmail.com>
2020-09-01 17:39:46 -0600

    added peramiters for name input

--- assets/js/robot-gladiators.js
+++ assets/js/robot-gladiators.js
@@ -67,7 +67,8 @@ var startGame = function() {
 
     for (var i = 0; i < enemyInfo.length; i++) {
         if (playerInfo.health > 0) {
-            window.alert("Welcome to Robot Gladiators! " + (i + 1) );
+            window.alert("Welcome to battlebots! Round " + (i + 1) );
+            debugger;
             var pickedEnemyObj = enemyInfo[i];
             pickedEnemyObj.health = randomNumber(40, 60);
         //debugger;
@@ -136,8 +137,17 @@ var randomNumber = function(min, max) {
     var value = Math.floor(Math.random() * (max - min + 1) + min);
     return value;
 };
+var getPlayerName = function() {
+    var name ="";
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+    console.log("Your robot's name is " + name);
+    return name;
+}
+
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,

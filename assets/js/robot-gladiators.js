c0dc5798ae3ceebc20af331172c7bac43b218866
maddiemoffat <maddiemoffat@hotmail.com>
2020-08-31 20:10:05 -0600

    random health and attack values

--- assets/js/robot-gladiators.js
+++ assets/js/robot-gladiators.js
@@ -14,7 +14,8 @@ var fight = function(enemyName) {
 
         if (promptFight === "fight" || promptFight === "FIGHT" || promptFight === "Fight") 
         {
-            enemyHealth = enemyHealth - playerAttack;
+            var damage = randomNumber(playerAttack - 3, playerAttack);
+            enemyHealth = Math.max(0, enemyHealth - damage);
             console.log(
                 playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
             );
@@ -29,7 +30,9 @@ var fight = function(enemyName) {
             }
 
             // remove player's health by subtracting the amount set in the enemyAttack variable
-            playerHealth = playerHealth - enemyAttack;
+            var damage = randomNumber(enemyAttack - 3, enemyAttack);
+            playerHealth = Math.max(0, playerHealth - damage);
+
             console.log(
                 enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
 
@@ -51,7 +54,7 @@ var fight = function(enemyName) {
             if (confirmSkip) {
               window.alert(playerName + " has decided to skip this fight. Goodbye!");
               // subtract money from playerMoney for skipping
-              playerMoney = playerMoney - 10;
+              playerMoney = math.max(0, playerMoney-10);
               console.log("playerMoney", playerMoney);
               break;
             }
@@ -74,16 +77,9 @@ var startGame = function() {
         if (playerHealth > 0) {
             window.alert("Welcome to Robot Gladiators! " + (i + 1) );
             var pickedEnemyName = enemyNames [i];
-            enemyHealth = 50;
+            enemyHealth = randomNumber(40, 60);
         //debugger;
         fight(pickedEnemyName);
-
-        if (playerHealth > 0 && i < enemyNames.length-1)
-            var storeConfrim = window.confirm("The fight is over, visit the store before the next round?");
-
-            if(storeConfrim) {
-                shop();
-            }
         }
         else {
             window.alert("You have lost your robot in balle! Game Over!");
@@ -106,69 +102,14 @@ var endGame = function () {
         else {
             window.alert("Thank you for playing Roboto Gladiators! come back soon!");
         }
-<<<<<<< HEAD
-        var playerAgainConfirm = window.confirm("Would you like to play again?");
-            if (playerAgainConfirm) {
-                startGame();
-            }
-            else {
-              window.alert("Thank you for playing Roboto Gladiators! come back soon!");
-            }
-    }  
-} 
-var shop = function() {
-    var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or Leave the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
-    )
-    switch (shopOptionPrompt) {
-        case "REFILL": 
-        case "refill":
-        case "Refill":    
-            if(playerMoney >= 7) { 
-                window.alert("Refilling player's health by 20 for 7 dollars.");
-                playerHealth = playerHealth + 20;
-                playerMoney = playerMoney - 7;
-            }
-          else {
-                window.alert("You don't have enough money!");
-          }
-            break;
-
-        case "upgrade":
-        case "UPGRADE":
-        case "Upgrade":        
-            if (playerMoney >= 7) {
-                window.alert("Upgrading player's attack by 6 for 7 dollars.");
-                playerAttack = playerAttack + 6;
-                playerMoney = playerMoney - 7;
-            }
-            else {
-                window.alert("You do not have enough money!");
-            }
-
-            break;
-
-        case "leave":
-        case "LEAVE":
-        case "Leave":        
-            window.alert("Leaving the store.");
-            break;
-        
-        default:
-            window.alert("You did not pick a valid option. Try again.")
-            
-            shop();
-            break;
-    }       
 }
-  startGame();
-=======
-}
-
 
 var shop = function() {
     console.log("entered the shop");
 };
 
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+    return value;
+};
 startGame();
->>>>>>> 8a32581596d65a007e6c806e8a0943ad5954981c
